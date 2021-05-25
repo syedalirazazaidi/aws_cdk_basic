57d7533b15c474ad0a3bf6791ff327d72b33d728
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-05-25 11:01:48 +0500

    Initial commit

---
+++ lib/13aws_a-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class 13AwsAStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
